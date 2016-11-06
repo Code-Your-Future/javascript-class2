533a54e442d09111e38692df694f487eaccb3ffa
Nour <nouraswad@hotmail.com>
2016-11-06 14:48:12 +0000

    Update buy_pizza.js

--- buy_pizza.js
+++ buy_pizza.js
@@ -1,1 +1,9 @@
 // code here
+var myMoney=prompt('How much do you have?');
+var pizzaPrice= 5;
+if (myMoney>5){
+	console.log('cool you can get at least one pizza');
+}else{
+	var moneyNeeded= 5 - myMoney;
+	console.log('you still need '+moneyNeeded+' $ to get at least one pizza');
+}
