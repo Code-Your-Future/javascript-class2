a66e62b13c92790b1f46ba827c84fdd4c2216b2f
ehmed-kino <ehmedaff@gmail.com>
2016-11-07 10:25:26 +0000

    adder-flip-light done

--- adder.js
+++ adder.js
@@ -1,1 +1,14 @@
-// code here
+// function add(num1, num2) {
+//   num1 = parseInt(prompt("something"));
+//   num2 = parseInt(prompt("something"));
+//   return num1 + num2;
+// }
+// function print(num) {
+//   document.write(num + "<br />");
+// }
+// print(add(654,65));
+// print(add("Hello ", "Ehmed!"));
+
+var a = parseInt(prompt());
+var b = parseInt(prompt());
+console.log(a + b);
--- light_switch.js
+++ light_switch.js
@@ -1,1 +1,17 @@
 // code here
+// number
+// var num = 3;
+// boolean
+// var lightOn = false;
+// charector
+// var car = "E";
+// string
+// var string = "Mohib is hansom";
+
+var lightOn = false;
+if(lightOn === false) {
+  document.write('the light on' + '</br >');
+}
+else {
+  document.write('the light off');
+}
