680e845904a2166cdf85eacd9ede2062bbef1066
ehmed-kino <ehmedaff@gmail.com>
2016-11-07 10:30:50 +0000

    adder modified

--- adder.js
+++ adder.js
@@ -1,14 +1,15 @@
-// function add(num1, num2) {
-//   num1 = parseInt(prompt("something"));
-//   num2 = parseInt(prompt("something"));
-//   return num1 + num2;
-// }
-// function print(num) {
-//   document.write(num + "<br />");
-// }
-// print(add(654,65));
-// print(add("Hello ", "Ehmed!"));
-
-var a = parseInt(prompt());
-var b = parseInt(prompt());
-console.log(a + b);
+function add(num1, num2) {
+  // num1 = parseInt(prompt("something"));
+  // num2 = parseInt(prompt("something"));
+  return num1 + num2;
+}
+function print(num) {
+  document.write(num + "<br />");
+}
+print(add(100,200));
+print(add("Hello ", "Ehmed!"));
+// you can do this as well
+// the prompt (INPUT) function return string so you have to parse it INT or FLOAT
+// var a = parseInt(prompt());
+// var b = parseInt(prompt());
+// console.log(a + b);
