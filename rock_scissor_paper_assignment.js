8cf75650e4b5487205e021484f7b79ce39ec9eba
Bridget Kalinda <bridgetkalinda@gmail.com>
2016-11-09 17:34:18 +0000

    r,s,p_game added

--- rock_scissor_paper_assignment.js
+++ rock_scissor_paper_assignment.js
@@ -1,1 +1,50 @@
-// code here
+var userTurn = prompt('Please choose rock, paper or scissors');
+var computerTurn = Math.random ();
+if (computerTurn <= 0.33) {
+	computerTurn = 'rock';
+}
+else if (0.34 <= computerTurn <= 0.66) {
+	computerTurn = 'scissors';
+}
+else {
+	computerTurn = 'paper';
+}
+
+turn1 = userTurn;
+turn2 = computerTurn;
+
+function compare(turn1, turn2) {
+	if (turn1 === turn2) {
+		return('The result is a tie!');
+	}
+
+	if (turn1 === 'rock') {
+		if (turn2 === 'scissors') {
+			return('rock beats scissors');
+		}
+		else {
+			return('paper wins');
+		}
+	}
+
+	if (turn1 === 'paper') {
+		if (turn2 === 'rock') {
+			return('paper beats rock');
+		}
+		else {
+			return('scissors wins');
+		}
+	}
+
+	if (turn1 === 'scissors') {
+		if (turn2 === 'rock') {
+			return('rock beats scissors');
+		}
+		else {
+			return('scissors wins');
+		}
+		
+	}
+}
+
+document.write(compare(turn1,turn2));
