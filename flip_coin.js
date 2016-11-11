6962b5dbb2b9d050ded6021bb1b3178eea572d7d
ArockiaAnsi <arockiaansi@gmail.com>
2016-11-11 23:44:11 +0000

    Update the Javascript codes

--- flip_coin.js
+++ flip_coin.js
@@ -1,1 +1,12 @@
 // code here
+document.write("Head Tail Exercise " + "<br />");
+//tail=0
+//head=1
+
+function flipCoin(){
+	return Math.floor(Math.random() *2);
+}
+while(flipCoin() === 0){
+	document.write("Tail, not lucky this time, trying again..." + "<br />");
+}
+document.write("We found a head" + "<br>");
--- light_switch.js
+++ light_switch.js
@@ -1,1 +1,14 @@
 // code here
+var lightSwitch = 'On';
+function light(lightSwitch){
+if (lightSwitch === 'On'){
+console.log( 'The Switch is ' +lightSwitch);
+}
+else {console.log ('The Switch is OFF!');
+}
+}
+light('On');
+light('Off');
+
+light('On');
+light('Off');
